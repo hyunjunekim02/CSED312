a55ab43d26cb1bbfaab30e6ffa9b755bc9664c13
hyunjunekim02 <hyunjunekim@postech.ac.kr>
2023-10-05 00:12:28 +0900

    fixed point type name created

--- src/threads/fixed_point.h
+++ src/threads/fixed_point.h
@@ -7,31 +7,34 @@ format:
 
 #include <stdint.h>
 
+//floating point type
+typedef int fp_t
+
 //floating point mecro
 #define FP (1 << 14)
 
 //fp operation functions
-int fp_convert_n_to_fp(int n);
-int fp_convert_x_to_integer_rounding_toward_zero(int x);
-int fp_convert_x_to_integer_rounding_to_nearest(int x);
-int fp_add(int x, int y);
-int fp_subtract_y_from_x(int x, int y);
-int fp_add_x_and_n(int x, int n);
-int fp_subtract_n_from_x(int n, int x);
-int fp_multiply_x_by_y(int x, int y);  //x should be int64_t
-int fp_multiply_x_by_n(int x, int n);
-int fp_divide_x_by_y(int x, int y);    //x should be int64_t
-int fp_divide_x_by_n(int x, int n);
+fp_t fp_n_to_fp(int n);
+int fp_x_to_int_round_zero(fp_t x);
+int fp_x_to_int_round_nearest(fp_t x);
+fp_t fp_add(fp_t x, fp_t y);
+fp_t fp_subtract_y_from_x(fp_t x, fp_t y);
+fp_t fp_add_x_and_n(fp_t x, int n);
+fp_t fp_subtract_n_from_x(int n, fp_t x);
+fp_t fp_multiply_x_by_y(fp_t x, fp_t y);  //x should be int64_t
+fp_t fp_multiply_x_by_n(fp_t x, int n);
+fp_t fp_divide_x_by_y(fp_t x, fp_t y);    //x should be int64_t
+fp_t fp_divide_x_by_n(fp_t x, int n);
 
-int fp_convert_n_to_fp(int n){
+fp_t fp_n_to_fp(int n){
     return n * FP;
 }
 
-int fp_convert_x_to_integer_rounding_toward_zero(int x){
+int fp_x_to_int_round_zero(fp_t x){
     return x / FP;
 }
 
-int fp_convert_x_to_integer_rounding_to_nearest(int x){
+int fp_x_to_int_round_nearest(fp_t x){
     if(x >= 0){
         return (x+FP/2)/FP;
     }
@@ -40,34 +43,34 @@ int fp_convert_x_to_integer_rounding_to_nearest(int x){
     }
 }
 
-int fp_add(int x, int y){
+fp_t fp_add(fp_t x, fp_t y){
     return x + y;
 }
 
-int fp_subtract_y_from_x(int x, int y){
+fp_t fp_subtract_y_from_x(fp_t x, fp_t y){
     return x - y;
 }
 
-int fp_add_x_and_n(int x, int n){
+fp_t fp_add_x_and_n(fp_t x, int n){
     return x + n * FP;
 }
 
-int fp_subtract_n_from_x(int n, int x){
+fp_t fp_subtract_n_from_x(int n, fp_t x){
     return x - n * FP;
 }
 
-int fp_multiply_x_by_y(int x, int y){
+fp_t fp_multiply_x_by_y(fp_t x, fp_t y){
     return ((int64_t)x) * y / FP;
 }   //x should be int64_t
 
-int fp_multiply_x_by_n(int x, int n){
+fp_t fp_multiply_x_by_n(fp_t x, int n){
     return x * n;
 }
 
-int fp_divide_x_by_y(int x, int y){
+fp_t fp_divide_x_by_y(fp_t x, fp_t y){
     return ((int64_t)x) * FP / y;
 }   //x should be int64_t
 
-int fp_divide_x_by_n(int x, int n){
+fp_t fp_divide_x_by_n(fp_t x, int n){
     return x / n;
 }
