10d28594831f2740edc44e467a5926fe20642571
hataehyeok <105369662+hataehyeok@users.noreply.github.com>
2023-11-26 11:49:31 +0900

    add omitted field

--- src/vm/page.h
+++ src/vm/page.h
@@ -15,23 +15,23 @@
 #include "lib/kernel/hash.h"
 // 뭐 인클루드 시켜야 되냐?
 
+#define VM_BIN 0
+#define VM_FILE 1
+#define VM_ANON 2
 
 struct list lru_list;
 
 struct vm_entry {
   uint8_t type;
-
+  void *vaddr;
   bool writable;
   bool is_loaded;
-  struct file* file;
-
-  struct list_elem mmap_elem;
   size_t offset;
   size_t read_bytes;
   size_t zero_bytes;
-
   size_t swap_slot;
-
+  struct file* file;
+  struct list_elem mmap_elem;
   struct hash_elem elem;
 };
 
